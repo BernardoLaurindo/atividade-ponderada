e9e35a8af062e9b3e88121aae9b73cf8eac8dcb9
Bernardo Laurindo <bernardo.gonzaga@sou.inteli.edu.br>
2025-02-18 11:32:24 -0300

    Update ponderada.js

--- ponderada.js
+++ ponderada.js
@@ -1,16 +1,17 @@
-let notas = [5, 6, 7, 10, 3];
-let somadasnotas = 0;
+let notas = [8, 6, 7, 10, 3.5];
 
-for (let i = 0; i < notas.length; i++) {
-    somadasnotas += notas[i];
+function calcularMedia(notas) {
+    let soma = 0;
+    for (let i = 0; i < notas.length; i++) {
+        soma += notas[i];
+    }
+    return soma / notas.length;
 }
 
-let media = somadasnotas / notas.length;
+let media = calcularMedia(notas);
 
 if (media >= 7) {
-    console.log("Aprovado a media foi de:" +media);
-    } 
-    else{ (media < 7) 
-    console.log("Reprovado a media foi de: " +media);
-
-    }
+    console.log("Aprovado! A média foi de: " + media);
+} else {
+    console.log("Reprovado! A média foi de: " + media); 
+}
